10065809e624f9a1ee19412fb7734a24b2fbd996
Joseph stein <josephaidenstein@gmail.com>
2025-09-27 14:31:44 -0400

    asdassd

--- scripts/Title.gd
+++ scripts/Title.gd
@@ -1,4 +1,4 @@
 extends MarginContainer
 
 func _on_click_me_button_pressed():
-	get_tree().quit() # This will quit the game when the button is clicked
+	get_tree().change_scene_to_file("res://scenes/lvl 1.tscn") # This will change the scene to the first level
